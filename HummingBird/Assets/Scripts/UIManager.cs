52b68cba9173e784dde67d76e6e7452912e0e982
hassan007farooq <41106213+hassan007farooq@users.noreply.github.com>
2020-07-14 19:00:26 +0500

    Done!!!

---
+++ HummingBird/Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void PenguineGame()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+
+    public void HummingBirdGame()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Game_Menu");
+    }
+}
